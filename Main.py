79c8b288fe7355a3c287e17a685f6b4788348254
Karthikeyasharma979 <90711330+Karthikeyasharma979@users.noreply.github.com>
2024-05-31 09:50:04 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -5,24 +5,12 @@ st.header("Diet Plan")
 # st.balloons()
 Name = st.text_input("Enter your name :")
 age = st.text_input("Enter your age ")
-height = st.text_input("Enter your height")
-weight = st.number_input("Enter your weight")
-
-
-choise = st.selectbox('Select option ',['select','Generate Diet Plan','Health calculate'])
-
-if choise == 'Generate Diet Plan':
-    st.text("What is your purpose?")
-    pur = st.text_area("Purpose",)
-       
-
-
-
+height = st.text_input("Enter your height (Feet)")
+weight = st.number_input("Enter your weight(kg)")
+Bp = st.number_input("Enter your Bp")
+sugar = st.number_input("Enter your sugar level")
+st.text("What is your purpose?")
+pur = st.text_area("Purpose",)
 str = st.button(label='Submit', key="submit")
-if str:
-    if choise == 'Generate Diet Plan':
-        result = Plan.plan(age=age,height=height,weight=weight,purpose=pur)
-        st.write(result)
-    if choise == 'Health calculate':
-        result = Plan.call(age=age,height=height,weight=weight)
-        st.write(result)
+result = Plan.plan(age=age,height=height,weight=weight,purpose=pur)
+st.write(result)
